95c41a05c63aed007d4f2c488971f327d0e41392
Adhikaxx88 <haowen88yt@gmail.com>
2026-02-05 01:12:27 +0700

    Update model training using GPU (TensorFlow) + Save .pkl

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,18 +1,26 @@
-from sklearn.base import BaseEstimator,TransformerMixin
-
-from sentence_transformers import SentenceTransformer
 import numpy as np
+from sentence_transformers import SentenceTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
 
-class URLEmbeeddingTransformer(BaseEstimator, TransformerMixin):
+class URLEmbeddingTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.model_name = model_name
-        self.encoder = None
-        
-    def fit(self, X,y=None):
-        self.encoder= SentenceTransformer(self.model_name)
-        return self
+        self.model = None
+
+    def fit(self, X, y=None):
+        return self  
+
     def transform(self, X):
+  
+        if self.model is None:
+            print(f"📥 Loading Model: {self.model_name}...")
+            self.model = SentenceTransformer(self.model_name)
+
         if isinstance(X, np.ndarray):
             X = X.flatten().tolist()
+
+        if isinstance(X, str):
+            X = [X]
             
-        return self.encoder.encode(X)
+        print("⚙️ Encoding URL...")
+        return self.model.encode(X)
