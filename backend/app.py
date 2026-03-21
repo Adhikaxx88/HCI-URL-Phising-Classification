1ab2a7e58060810ca851e8ad71891a74b6426ea3
Adhikaxx88 <haowen88yt@gmail.com>
2026-03-21 20:50:13 +0700

    backend online on hugging face

--- backend/app.py
+++ backend/app.py
@@ -76,3 +76,7 @@ def predict(request: URLRequest):
         }
     except Exception as e:
         return {"error": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7860)
